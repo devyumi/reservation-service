b7b42306ded078cfd9a5b1ab0b3cda4ce24a4fd2
devyumi <yyumiwing@naver.com>
2023-10-22 22:53:43 +0900

    refactor: 경로 수정

--- reservation/src/main/java/kr/or/connect/reservation/controller/ReservationController.java
+++ reservation/src/main/java/kr/or/connect/reservation/controller/ReservationController.java
@@ -1,13 +1,9 @@
 package kr.or.connect.reservation.controller;
 
-import java.util.Collections;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,35 +15,36 @@ import kr.or.connect.reservation.dto.PromotionDto;
 import kr.or.connect.reservation.service.ReservationService;
 
 @RestController
+@RequestMapping("api/v1")
 public class ReservationController {
 
 	@Autowired
 	ReservationService reservationService;
 	
-	@GetMapping("api/categories")
+	@GetMapping("categories")
 	public CategoryDto findCategories() {
-		return reservationService.findCategory();
+		return reservationService.findCategories();
 	}
 	
-	@GetMapping("api/displayinfos")
+	@GetMapping("displayinfos")
 	public DisplayInfoDto findDisplayInfos(@RequestParam(name = "categoryId", required = false, defaultValue = "0") int categoryId,
 											@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
-		return reservationService.findDisplayInfo(categoryId, start);
+		return reservationService.findDisplayInfos(categoryId, start);
 	}
 	
-	@GetMapping("api/promotions")
+	@GetMapping("promotions")
 	public PromotionDto findPromotions() {
-		return reservationService.findPromotion();
+		return reservationService.findPromotions();
 	}
 	
-	@GetMapping("api/displayinfos/{displayId}")
+	@GetMapping("displayinfos/{displayId}")
 	public DisplayInfoIdDto findDisplayInfoIds(@PathVariable(name = "displayId") int displayInfoId) {
-		return reservationService.findDisplayInfoId(displayInfoId);
+		return reservationService.findDisplayInfoIds(displayInfoId);
 	}
 	
-	@GetMapping("api/comments")
+	@GetMapping("comments")
 	public CommentDto findComments(@RequestParam(name = "productId", required = false, defaultValue = "0") int productId,
 									@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
-		return reservationService.findComment(productId, start);
+		return reservationService.findComments(productId, start);
 	}
 }
