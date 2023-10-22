30c823856a2d41a34b74528abbcbfb11bf54de85
devyumi <yyumiwing@naver.com>
2023-10-23 01:55:46 +0900

    refactor: Correct promotion-api's sql

--- reservation/src/main/java/kr/or/connect/reservation/dao/ReservationDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/ReservationDaoSqls.java
@@ -33,6 +33,7 @@ public class ReservationDaoSqls {
 			+ "ON product.category_id = category.id "
 			+ "INNER JOIN product_image "
 			+ "ON product.id = product_image.product_id "
+			+ "WHERE type = 'ma' "
 			+ "GROUP BY promotion.id";
 	
 	public static final String SELECT_DISPLAY_INFO = "SELECT product.id, category.id as categoryId, display_info.id as displayInfoId, name, description, content, event, opening_hours as openingHours, place_name as placeName, place_lot as placeLot, place_street as placeStreet, tel, homepage, email, display_info.create_date as createDate, display_info.modify_date as modifyDate, product_image.file_id as fileId "
