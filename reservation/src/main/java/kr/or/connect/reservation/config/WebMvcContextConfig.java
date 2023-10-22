717d7bc72de7d49d5b498c5ee99007370792fa29
devyumi <yyumiwing@naver.com>
2023-10-22 22:38:01 +0900

    refactor: set swagger

--- reservation/src/main/java/kr/or/connect/reservation/config/WebMvcContextConfig.java
+++ reservation/src/main/java/kr/or/connect/reservation/config/WebMvcContextConfig.java
@@ -5,39 +5,40 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import io.swagger.annotations.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableWebMvc
+@EnableSwagger2
 @ComponentScan(basePackages = {"kr.or.connect.reservation.controller"})
 public class WebMvcContextConfig extends WebMvcConfigurerAdapter{
 	
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
-    }
- 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-   
-    @Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-    		System.out.println("addViewControllers가 호출됩니다. ");
-        registry.addViewController("/").setViewName("index");
-    }
-    
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/api/**"))
+				.build()
+				.apiInfo(apiInfo())
+				.useDefaultResponseMessages(false);
+	}
+	
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("Reservation-service", "", "", "", "", "", "");
+		return apiInfo;
+	}
 }
