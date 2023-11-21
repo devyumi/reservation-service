cd6eaffb73053ffb2bd4b7f2578852c28f51a7f3
devyumi <yyumiwing@naver.com>
2023-11-21 19:58:12 +0900

    refactor: change the sql

--- reservation/src/main/java/kr/or/connect/reservation/dao/UserDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/UserDaoSqls.java
@@ -1,5 +1,5 @@
 package kr.or.connect.reservation.dao;
 
 public class UserDaoSqls {
-	public static final String SELECT_ALL_BY_EMAIL = "SELECT id, name, password, email, create_date, modify_date FROM member WHERE email = :email";
+	public static final String SELECT_ALL_BY_EMAIL = "SELECT id, name, password, email, phone, create_date, modify_date FROM user WHERE email = :email";
 }
